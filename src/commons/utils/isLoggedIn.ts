e04ba9307c32492069e6024334df39403911e038
hyejin <yhj970102@gmail.com>
2024-03-10 22:27:15 +0900

    ✨ Feat : getCookie, isLoggedIn 유틸함수 생성

---
+++ src/commons/utils/isLoggedIn.ts
@@ -0,0 +1,8 @@
+// import { getCookie } from '@/commons/utils//getCookie';
+
+export const isLoggedIn = () => {
+  // const accessToken = getCookie('accessToken');
+
+  // if (accessToken) return true;
+  return false;
+};

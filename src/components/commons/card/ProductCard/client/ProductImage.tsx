0a62c82c0527d4bfed4cde7d9dac9d13c5b4c138
hyejin <yhj970102@gmail.com>
2024-03-15 17:13:12 +0900

    ✨ Feat : 클라이언트 컴포넌트에서 atom isLoggedIn 값을 가져와 로그인 여부 판별하기

--- src/components/commons/card/ProductCard/client/ProductImage.tsx
+++ src/components/commons/card/ProductCard/client/ProductImage.tsx
@@ -1,8 +1,13 @@
+'use client';
+
+import { Dispatch, SetStateAction } from 'react';
+import { useRecoilValue } from 'recoil';
+import { useRouter } from 'next/navigation';
+import { isLoggedInState } from '@/atoms/atom';
 import usePopup from '@/commons/hooks/usePopup';
 import { IProductType } from '@/commons/types/productType';
 import BtnHeart from '@/components/commons/button/client/Btn_heart';
 import ToastPop from '@/components/commons/ToastPop';
-import { Dispatch, SetStateAction } from 'react';
 
 interface ProductImageProps {
   product: IProductType;
@@ -11,9 +16,13 @@ interface ProductImageProps {
 }
 export const ProductImage = ({ product, setIsModal, setProductId }: ProductImageProps) => {
   const { openPopup } = usePopup();
+  const router = useRouter();
+  const isLoggedIn = useRecoilValue(isLoggedInState);
 
   const handleClickHeart = (id: number) => (e: any) => {
     e.preventDefault();
+
+    if (!isLoggedIn) return router.push('/');
 
     if (product.isWished) {
       openPopup(

70b969c7d6a708405100043e2a2efb6dbc225301
hyejin <yhj970102@gmail.com>
2024-04-11 21:19:47 +0900

    ♻️ Refactor : window 관련 if 문 삭제

--- src/global/RegisterServiceWorker.tsx
+++ src/global/RegisterServiceWorker.tsx
@@ -4,12 +4,10 @@ import { useEffect } from 'react';
 
 const RegisterServiceWorker = () => {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js').then((registration) => {
-          console.info('Service Worker registration successful:', registration);
-        });
-      }
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').then((registration) => {
+        console.info('Service Worker registration successful:', registration);
+      });
     }
   }, []);
 

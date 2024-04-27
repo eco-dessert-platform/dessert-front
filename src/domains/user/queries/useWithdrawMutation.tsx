da0c804080f616ce57db959660ea74e48bd3e2e3
jijiseong <jiseong5036@gmail.com>
2024-04-27 17:53:26 +0900

    ♻️ Refactor :  회원 탈퇴 API 공통 응답 리팩토링

--- src/domains/user/queries/useWithdrawMutation.tsx
+++ src/domains/user/queries/useWithdrawMutation.tsx
@@ -1,29 +1,60 @@
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
-import { withdrawMemberShip } from '@/domains/user/queries/withdrawMemberShip';
 import usePopup from '@/shared/hooks/usePopup';
 import useToast from '@/shared/hooks/useToast';
 import ToastPop from '@/shared/components/ToastPop';
+import PATH from '@/shared/constants/path';
+import fetchExtend from '@/shared/utils/api';
+import { ResultResponse } from '@/shared/types/response';
+
+interface WithdrawResponse {
+  message: string;
+}
 
 const useWithdrawMutation = () => {
   const router = useRouter();
   const { closePopup } = usePopup();
   const { openToast } = useToast();
 
+  const mutationFn = async ({ formData }: { formData: FormData }) => {
+    const rawFormData = {
+      reasons: formData.getAll('delete-reason').join(',')
+    };
+
+    const res = await fetchExtend.patch('/members', {
+      body: JSON.stringify(rawFormData)
+    });
+    const { result, success, code }: ResultResponse<WithdrawResponse> = await res.json();
+
+    if (!res.ok || !success) {
+      throw new Error(`[ERROR ${code}] 회원 탈퇴 중 에러가 발생했어요.`);
+    }
+
+    return result;
+  };
+
+  const onSuccess = ({ message }: { message: string }) => {
+    openToast(
+      <ToastPop>
+        <div>{message}</div>
+      </ToastPop>
+    );
+    router.push(PATH.home);
+  };
+
+  const onError = ({ message }: Error) => {
+    openToast(
+      <ToastPop>
+        <div>{message}</div>
+      </ToastPop>
+    );
+  };
+
   return useMutation({
-    mutationFn: ({ formData }: { formData: FormData }) => withdrawMemberShip({ formData }),
+    mutationFn,
     onSettled: closePopup,
-    onSuccess: (message: string) => {
-      openToast(
-        <ToastPop>
-          <div>{message}</div>
-        </ToastPop>
-      );
-      router.push('/');
-    },
-    // onError: (errorMessage: Error['message']) => {
-    //   console.log('회원탈퇴 에러 발생:', errorMessage);
-    // },
+    onSuccess,
+    onError,
     throwOnError: true
   });
 };

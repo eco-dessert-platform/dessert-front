dcebac1c9db719d4f96ff3a9773f6738d68da6d2
H0onnn <dukei2017@naver.com>
2024-04-29 18:08:48 +0900

    🛠️ Modify : 변수명, 버튼 텍스트 변경

--- src/domains/user/components/RecommendForm/ButtonSection.tsx
+++ src/domains/user/components/RecommendForm/ButtonSection.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { useRecoilValue } from 'recoil';
-import { recommendState } from '@/domains/user/atoms/profile';
+import { personalizedRecommendationState } from '@/domains/user/atoms/profile';
 import Button from '@/shared/components/Button';
 
 const ButtonSection = () => {
-  const checkedValue = useRecoilValue(recommendState);
+  const checkedValue = useRecoilValue(personalizedRecommendationState);
 
   return (
     <div className="fixed w-full max-w-[600px] px-[16px] transform -translate-x-1/2 bottom-24 left-1/2">
@@ -17,7 +17,7 @@ const ButtonSection = () => {
           disabled:bg-gray-300 disabled:text-white
       `}
       >
-        다음
+        완료
       </Button>
     </div>
   );

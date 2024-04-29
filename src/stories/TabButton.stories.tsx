501c1000062ac7433e8d2e300307ab65b992dacc
H0onnn <dukei2017@naver.com>
2024-04-29 11:04:11 +0900

    ✨ Feat : TabButton 스토리 decorators 추가

--- src/stories/TabButton.stories.tsx
+++ src/stories/TabButton.stories.tsx
@@ -5,7 +5,14 @@ import TabButton from '@/shared/components/TabButton';
 
 const meta: Meta<typeof TabButton> = {
   component: TabButton,
-  title: 'TabButton'
+  title: 'TabButton',
+  decorators: [
+    (Story) => (
+      <div className="w-full max-w-[600px] h-screen m-auto shadow-lg shadow-slate-100">
+        <Story />
+      </div>
+    )
+  ]
 };
 
 export default meta;
@@ -16,7 +23,7 @@ const Template = () => {
   const [isActive, setIsActive] = useState(true);
 
   return (
-    <div className="flex">
+    <div className="flex w-full">
       <TabButton active={isActive} onClick={() => setIsActive(true)}>
         상품별
       </TabButton>

2e6bde4d96ceafbab8daa60bd8003493ef2e4c62
H0onnn <dukei2017@naver.com>
2024-04-29 12:42:14 +0900

    ♻️ Refactor : 제품 상세 페이지 API 리팩토링

--- src/app/(main)/(detail)/products/[id]/page.tsx
+++ src/app/(main)/(detail)/products/[id]/page.tsx
@@ -8,11 +8,12 @@ import GrayDivider from '@/shared/components/GrayDivider';
 import { IProductDetailType } from '@/domains/product/types/productDetailType';
 import DetailStoreInfo from '@/domains/store/components/DetailStoreInfo';
 import fetchExtend from '@/shared/utils/api';
+import { ResultResponse } from '@/shared/types/response';
 
 async function getDetail(params: { id: string }) {
   const res = await fetchExtend.get(`/boards/${params.id}`);
-  const data: IProductDetailType = await res.json();
-  return data;
+  const { result }: ResultResponse<IProductDetailType> = await res.json();
+  return result;
 }
 
 const ProductDetail = async ({ params }: { params: { id: string } }) => {

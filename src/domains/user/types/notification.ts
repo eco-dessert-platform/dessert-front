ad52bd3c5116a13b321897e81c7540d8a5321f2d
jijiseong <jiseong5036@gmail.com>
2024-05-04 23:38:39 +0900

    ♻️ Refactor :  공지사항 API 리팩토링

--- src/domains/user/types/notification.ts
+++ src/domains/user/types/notification.ts
@@ -1,10 +1,5 @@
-interface NotificationType {
+export interface NotificationType {
   id: number;
   title: string;
   createdAt: string;
-}
-
-export interface AllNotificationsType {
-  contents: Array<NotificationType>;
-  lastPage: number;
 }

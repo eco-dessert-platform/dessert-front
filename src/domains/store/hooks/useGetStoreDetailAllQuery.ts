634b0c530cc027ba866ba5ab8a998b0b7a543642
Wlfjd <leejiwon813@naver.com>
2024-05-13 23:20:32 +0900

    ♻️ Refactor :  불필요 코드 삭제

--- src/domains/store/hooks/useGetStoreDetailAllQuery.ts
+++ src/domains/store/hooks/useGetStoreDetailAllQuery.ts
@@ -7,7 +7,6 @@ import { IStoreDetailAllType } from '../types/storeDetailType';
 
 const getStoreDetailAll = async (id: number) => {
   const res = await fetchExtend.get(`/stores/${id}/boards/all?page=0`);
-  if (!res.ok) throw new Error('상품 상세 조회 실패');
   const { success, result, code, message }: ResultResponse<IStoreDetailAllType> = await res.json();
   if (!res.ok || !success) {
     throwApiError({ code, message });

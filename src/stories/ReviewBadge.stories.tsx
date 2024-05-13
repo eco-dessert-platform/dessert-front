2b9014006e46c87775bbb2b8b4b6ad83261c7ed9
hyejin <yhj970102@gmail.com>
2024-05-13 20:12:36 +0900

    ♻️ Refactor : `ReviewBadge` 스토리 수정

--- src/stories/ReviewBadge.stories.tsx
+++ src/stories/ReviewBadge.stories.tsx
@@ -12,41 +12,37 @@ const meta: Meta<typeof ReviewBadge> = {
 export default meta;
 type Story = StoryObj<typeof ReviewBadge>;
 
-const Template = ({ isActive = false }: { isActive?: boolean }) => {
-  const badgeShapes = Object.keys(BADGE) as Array<BadgeShapeType>;
+const BADGE_SHAPES = Object.keys(BADGE) as Array<BadgeShapeType>;
 
-  return (
-    <div className="grid grid-rows-3 grid-cols-2 gap-[10px] max-w-[600px]">
-      {badgeShapes.map((shape) => (
-        <ReviewBadge shape={shape} isActive={isActive} />
-      ))}
-    </div>
-  );
-};
+const Template = ({ isActive = false }: { isActive?: boolean }) => (
+  <div className="grid grid-rows-3 grid-cols-2 gap-[10px] max-w-[600px]">
+    {BADGE_SHAPES.map((shape) => (
+      <ReviewBadge shape={shape} isActive={isActive} />
+    ))}
+  </div>
+);
 
 const ClickStory = () => {
-  const badgeShapes = Object.keys(BADGE) as Array<BadgeShapeType>;
   const [activeBadge, setActiveBadge] = useState({
     preference: undefined,
     taste: undefined,
     texture: undefined
   });
 
-  const handleClick = (shape: BadgeShapeType) => {
-    const kind = BADGE[shape].kind as BadgeKindType;
+  const handleClick = (kind: BadgeKindType, shape: BadgeShapeType) => {
     setActiveBadge({ ...activeBadge, [kind]: shape });
   };
 
   return (
     <div className="grid grid-rows-3 grid-cols-2 gap-[10px] max-w-[600px]">
-      {badgeShapes.map((shape) => {
+      {BADGE_SHAPES.map((shape) => {
         const kind = BADGE[shape].kind as BadgeKindType;
         return (
           <ReviewBadge
             shape={shape}
             isActive={activeBadge[kind] === shape}
             className="cursor-pointer"
-            onClick={() => handleClick(shape)}
+            onClick={() => handleClick(kind, shape)}
           />
         );
       })}

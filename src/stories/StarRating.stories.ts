626fe387ac182f798b51267adb4d74dfa08f2f14
hyejin <yhj970102@gmail.com>
2024-05-14 20:50:11 +0900

    📖 Storybook : `StarRating` 스토리 생성

---
+++ src/stories/StarRating.stories.ts
@@ -0,0 +1,22 @@
+import StarRating from '@/domains/review/components/StarRating';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof StarRating> = {
+  component: StarRating,
+  title: 'StarRating'
+};
+export default meta;
+
+type Story = StoryObj<typeof StarRating>;
+
+export const Default: Story = {
+  args: { initRating: 3.5 }
+};
+
+export const Size: Story = {
+  args: { initRating: 2, size: 'large' }
+};
+
+export const Drag: Story = {
+  args: { initRating: 0, size: 'large', dragged: true }
+};

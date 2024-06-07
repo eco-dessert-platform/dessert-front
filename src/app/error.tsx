e1c14e20925bd2f808b3872315fa5ba13cd45216
jijiseong <jiseong5036@gmail.com>
2024-06-07 16:01:42 +0900

    design: layout css, error page css

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,8 +3,9 @@
 import Button from '@/shared/components/Button';
 import PaddingWrapper from '@/shared/components/PaddingWrapper';
 import SadBbangleBox from '@/shared/components/SadBbangleBox';
-import Link from 'next/link';
+import { BbangleIcon } from '@/shared/components/icons';
 import PATH from '@/shared/constants/path';
+import Link from 'next/link';
 import { useEffect } from 'react';
 
 const Error = ({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) => {
@@ -13,23 +14,22 @@ const Error = ({ error, reset }: { error: Error & { digest?: string }; reset: ()
   });
 
   return (
-    <PaddingWrapper className="flex flex-wrap gap-x-[4%] gap-y-4">
-      <SadBbangleBox className="h-[80vh]">
-        <p>에러가 발생했어요!</p>
-      </SadBbangleBox>
-      <div className="justify-evenly bg-white w-full max-w-[600px] mx-auto p-[16px] fixed flex gap-[10px] left-0 right-0 bottom-0 z-[5000]">
-        <div className="flex-1">
-          <Button onClick={reset} variants="primary-black">
+    <>
+      <PaddingWrapper>
+        <Link href={PATH.home}>
+          <BbangleIcon shape="horizontal-name" />
+        </Link>
+      </PaddingWrapper>
+      <PaddingWrapper className="absoulte-center flex flex-col">
+        <SadBbangleBox>
+          <div>일시적인 오류가 발생했어요.</div>
+          <div>다시 시도해주세요.</div>
+          <Button className="mt-[16px]" onClick={reset} variants="primary-black">
             다시 시도하기
           </Button>
-        </div>
-        <div className="flex-1">
-          <Link href={PATH.home}>
-            <Button variants="primary-white">홈으로 가기</Button>
-          </Link>
-        </div>
-      </div>
-    </PaddingWrapper>
+        </SadBbangleBox>
+      </PaddingWrapper>
+    </>
   );
 };
 

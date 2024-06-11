4800b240148dd044d47a6feb36396af80d4e6260
Wlfjd <leejiwon813@naver.com>
2024-06-11 13:33:24 +0900

    ✨ Feat :  주문가능상품만 보기 추가, 초기화 버튼 추가

--- src/domains/product/components/alert-box/FilterModal/index.tsx
+++ src/domains/product/components/alert-box/FilterModal/index.tsx
@@ -1,25 +1,28 @@
 'use client';
 
+import ButtonSection from '@/domains/product/components/alert-box/FilterModal/ButtonSection';
+import CategorySection from '@/domains/product/components/alert-box/FilterModal/CategorySection';
+import PriceSection from '@/domains/product/components/alert-box/FilterModal/PriceSection';
+import TagsSection from '@/domains/product/components/alert-box/FilterModal/TagsSection';
 import { FilterFamilyIDType } from '@/domains/product/types/filterType';
 import Modal from '@/shared/components/Modal';
-import TagsSection from '@/domains/product/components/alert-box/FilterModal/TagsSection';
-import PriceSection from '@/domains/product/components/alert-box/FilterModal/PriceSection';
-import CategorySection from '@/domains/product/components/alert-box/FilterModal/CategorySection';
-import ButtonSection from '@/domains/product/components/alert-box/FilterModal/ButtonSection';
+
+import IsOrderAvailableCheckbox from './IsOrderAvailableCheckbox';
 
 interface FilterModalProps {
   filterFamilyId: FilterFamilyIDType;
 }
 
 const FilterModal = ({ filterFamilyId }: FilterModalProps) => (
-    <Modal title="필터">
-      <CategorySection filterFamilyId={filterFamilyId} />
-      <hr className="bg-gray-100" />
-      <TagsSection filterFamilyId={filterFamilyId} />
-      <hr className="bg-gray-100" />
-      <PriceSection filterFamilyId={filterFamilyId} />
-      <ButtonSection filterFamilyId={filterFamilyId} />
-    </Modal>
-  )
+  <Modal title="필터">
+    <TagsSection filterFamilyId={filterFamilyId} />
+    <hr className="bg-gray-100" />
+    <CategorySection filterFamilyId={filterFamilyId} />
+    <hr className="bg-gray-100" />
+    <PriceSection filterFamilyId={filterFamilyId} />
+    <IsOrderAvailableCheckbox filterFamilyId={filterFamilyId} />
+    <ButtonSection filterFamilyId={filterFamilyId} />
+  </Modal>
+);
 
 export default FilterModal;
--- src/domains/product/constants/filterValues.ts
+++ src/domains/product/constants/filterValues.ts
@@ -1,8 +1,8 @@
-import { LIMIT_MIN_PRICE, LIMIT_MAX_PRICE } from '@/domains/product/constants/priceLimit';
+import { LIMIT_MAX_PRICE, LIMIT_MIN_PRICE } from '@/domains/product/constants/priceLimit';
 
 export const FILTER_VALUES = {
   categories: ['전체', '빵', '쿠키', '케이크', '타르트', '잼/청', '요거트', '기타'],
-  tags: ['전체', '글루텐프리', '고단백', '비건', '무설탕', '키토제닉']
+  tags: ['전체', '글루텐프리', '고단백', '비건', '무설탕', '저지방']
 };
 
 export const INIT_FILTER_VALUE = {
@@ -10,5 +10,5 @@ export const INIT_FILTER_VALUE = {
   tags: undefined,
   price: [LIMIT_MIN_PRICE, LIMIT_MAX_PRICE],
   sort: '추천순',
-  showProductsAvailableOrder: false
+  isOrderAvailable: false
 };

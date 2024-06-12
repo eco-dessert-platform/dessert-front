948a6109b40b03a68b40b654bd9c09f8c4f15335
Wlfjd <leejiwon813@naver.com>
2024-06-12 21:11:36 +0900

    💄 Design:  padding 값 수정

--- src/blocks/main/(detail)/products/[productId]/info/BoardDetailSection.tsx
+++ src/blocks/main/(detail)/products/[productId]/info/BoardDetailSection.tsx
@@ -10,7 +10,7 @@ const BoardDetailsSection = async ({ productId }: Props) => {
   const { boardDetails } = await productService.getBoardDetail(productId);
 
   return (
-    <div className="w-full p-0 ">
+    <div className="w-full p-0 pt-[16px]">
       {boardDetails.map((item) => (
         <Image key={item} src={item} alt="상세" width={600} height={100} className=" m-auto" />
       ))}

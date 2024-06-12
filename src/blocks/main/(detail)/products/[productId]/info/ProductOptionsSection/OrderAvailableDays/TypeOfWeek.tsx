f450495efddefaee2558ca8e8c5db0daaccced46
Wlfjd <leejiwon813@naver.com>
2024-06-12 19:26:18 +0900

    💄 Design:  TypeOfWeek typo 수정

--- src/blocks/main/(detail)/products/[productId]/info/ProductOptionsSection/OrderAvailableDays/TypeOfWeek.tsx
+++ src/blocks/main/(detail)/products/[productId]/info/ProductOptionsSection/OrderAvailableDays/TypeOfWeek.tsx
@@ -10,9 +10,9 @@ const TypeOfWeek = ({ availableDays }: { availableDays: Object }) => {
       {['월', '화', '수', '목', '금', '토', '일'].map((item) => (
         <div
           key={item}
-          className={`rounded-full leading-150 w-[24px] h-[24px] text-12 font-normal flex items-center justify-center  ${
+          className={`rounded-full typo-body-12-regular w-[24px] h-[24px] flex items-center justify-center  ${
             availableWeekList.includes(transformDayTag(item))
-              ? 'text-primaryOrangeRed bg-subColorPink font-medium'
+              ? 'text-primaryOrangeRed bg-subColorPink typo-body-12-medium'
               : 'text-gray-600 bg-gray-50 '
           }`}
         >

aaaf1784298bf6fed19de9d3688f5125eaebaba5
hyejin <yhj970102@gmail.com>
2024-06-26 17:24:40 +0900

    ✨ Feat :  조사 체크 함수 생성

--- src/domains/home/utils/genGuidanceMessage.ts
+++ src/domains/home/utils/genGuidanceMessage.ts
@@ -1,13 +1,14 @@
 import { PreferenceType } from '@/domains/user/types/preference';
 import { getTagsByPreference } from '@/domains/user/utils/getTagsByPreference';
+import { checkPostPosition } from '@/shared/utils/checkPostPosition';
 
 export const genGuidanceMessage = (preference: Array<PreferenceType>) => {
   switch (preference.length) {
     case 1:
-      return `${preference[0]}를 위한 ${getTagsByPreference(preference[0]).join('/')} 상품을 모아봤어요!`;
+      return `${checkPostPosition(preference[0], '를')} 위한 ${getTagsByPreference(preference[0]).join('/')} 상품을 모아봤어요!`;
     case 2:
-      return `${preference[0]}를 위한 ${getTagsByPreference(preference[0]).join('/')} 상품과
-      ${preference[1]}를 위한 ${getTagsByPreference(preference[1]).join('/')} 상품을 모아봤어요!`;
+      return `${checkPostPosition(preference[0], '를')} 위한 ${getTagsByPreference(preference[0]).join('/')} 상품과
+      ${checkPostPosition(preference[1], '를')} 위한 ${getTagsByPreference(preference[1]).join('/')} 상품을 모아봤어요!`;
     default:
       return null;
   }

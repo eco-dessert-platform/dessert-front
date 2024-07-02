10e7ea15c33e57aba275e3a392154e98150632b4
hyejin <yhj970102@gmail.com>
2024-07-02 12:57:32 +0900

    ♻️ Refactor : `checkPostPosition` -> `attachPostPosition` 함수명 변경

--- src/domains/home/utils/genGuidanceMessage.ts
+++ src/domains/home/utils/genGuidanceMessage.ts
@@ -1,14 +1,14 @@
 import { PreferenceType } from '@/domains/user/types/preference';
 import { getTagsByPreference } from '@/domains/user/utils/getTagsByPreference';
-import { checkPostPosition } from '@/shared/utils/checkPostPosition';
+import { attachPostPosition } from '@/shared/utils/attachPostPosition';
 
 export const genGuidanceMessage = (preference: Array<PreferenceType>) => {
   switch (preference.length) {
     case 1:
-      return `${checkPostPosition(preference[0], '를')} 위한 ${getTagsByPreference(preference[0]).join('/')} 상품을 모아봤어요!`;
+      return `${attachPostPosition(preference[0], '를')} 위한 ${getTagsByPreference(preference[0]).join('/')} 상품을 모아봤어요!`;
     case 2:
-      return `${checkPostPosition(preference[0], '를')} 위한 ${getTagsByPreference(preference[0]).join('/')} 상품과
-      ${checkPostPosition(preference[1], '를')} 위한 ${getTagsByPreference(preference[1]).join('/')} 상품을 모아봤어요!`;
+      return `${attachPostPosition(preference[0], '를')} 위한 ${getTagsByPreference(preference[0]).join('/')} 상품과
+      ${attachPostPosition(preference[1], '를')} 위한 ${getTagsByPreference(preference[1]).join('/')} 상품을 모아봤어요!`;
     default:
       return null;
   }

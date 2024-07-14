014f8ca1cba321cdeb996af720bc73f0391db35a
jijiseong <jiseong5036@gmail.com>
2024-07-14 15:56:23 +0900

    fix: app router mock 추가

--- src/domains/review/components/Review/index.test.tsx
+++ src/domains/review/components/Review/index.test.tsx
@@ -1,23 +1,27 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
+import { AppRouterContextProviderMock } from '@/__tests__/utils/AppRouterContextProviderMock';
 import Review from '.';
 
 describe('<Review /> 테스트', () => {
   test('필요한 정보가 노출된다.', () => {
+    const push = vi.fn();
     render(
-      <Review
-        id={1}
-        boardId={1}
-        nickname="닉네임"
-        rating={0}
-        comment="댓글"
-        tags={['태그1', '태그2']}
-        like={100}
-        images={[{ id: 1, url: 'https://test.jpg' }]}
-        isLiked={false}
-        isBest={false}
-        date="2024. 01. 01"
-      />
+      <AppRouterContextProviderMock router={{ push }}>
+        <Review
+          id={1}
+          boardId={1}
+          nickname="닉네임"
+          rating={0}
+          comment="댓글"
+          tags={['태그1', '태그2']}
+          like={100}
+          images={[{ id: 1, url: 'https://test.jpg' }]}
+          isLiked={false}
+          isBest={false}
+          date="2024. 01. 01"
+        />
+      </AppRouterContextProviderMock>
     );
 
     expect(screen.getByText('닉네임')).toBeDefined();

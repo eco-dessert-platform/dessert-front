e3413ec5ce4d5b64446079695676fdfa80862447
jijiseong <jiseong5036@gmail.com>
2024-07-29 10:56:33 +0900

    ✨ Feat :  ImageWithFallback 컴포넌트

---
+++ src/shared/components/ImageWithFallback/index.tsx
@@ -0,0 +1,24 @@
+import Image, { ImageProps } from 'next/image';
+import { ReactNode, useState } from 'react';
+
+interface Props extends ImageProps {
+  fallback: ReactNode;
+}
+
+const ImageWithFallback = ({ fallback, ...props }: Props) => {
+  const [isError, setIsError] = useState(false);
+
+  return isError ? (
+    fallback
+  ) : (
+    <Image
+      onError={() => {
+        console.log('test');
+        setIsError(true);
+      }}
+      {...props}
+    />
+  );
+};
+
+export default ImageWithFallback;

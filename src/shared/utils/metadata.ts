867d2e883553243fb36063d394495c2e8733d932
hyejin <yhj970102@gmail.com>
2024-09-04 15:26:51 +0900

    ✨ Feat :  metadata에 `twitter` 추가

--- src/shared/utils/metadata.ts
+++ src/shared/utils/metadata.ts
@@ -23,12 +23,23 @@ const staticMetadata: StaticMetadataType = {
       title: '빵그리의 오븐',
       description:
         '빵그리의 오븐은 건강을 소중히 여기는 이들에게 새로운 디저트 경험을 선사하고, 건강과 맛을 모두 충족시킬 수 있는 특별한 공간을 제공합니다.'
+    },
+    twitter: {
+      card: 'summary',
+      title: '빵그리의 오븐',
+      description:
+        '빵그리의 오븐은 건강을 소중히 여기는 이들에게 새로운 디저트 경험을 선사하고, 건강과 맛을 모두 충족시킬 수 있는 특별한 공간을 제공합니다.'
     }
   },
   search: {
     title: '검색',
     description: '빵그리의 오븐 검색 기능으로 원하는 상품을 찾아보실 수 있습니다.',
     openGraph: {
+      title: '검색 | 빵그리의 오븐',
+      description: '빵그리의 오븐 검색 기능으로 원하는 상품을 찾아보실 수 있습니다.'
+    },
+    twitter: {
+      card: 'summary',
       title: '검색 | 빵그리의 오븐',
       description: '빵그리의 오븐 검색 기능으로 원하는 상품을 찾아보실 수 있습니다.'
     }
@@ -40,6 +51,11 @@ const staticMetadata: StaticMetadataType = {
     openGraph: {
       title: '카테고리 | 빵그리의 오븐',
       description: '카테고리별로 건강한 디저트를 확인하세요.'
+    },
+    twitter: {
+      card: 'summary',
+      title: '카테고리 | 빵그리의 오븐',
+      description: '카테고리별로 건강한 디저트를 확인하세요.'
     }
   },
   wish: {
@@ -48,12 +64,22 @@ const staticMetadata: StaticMetadataType = {
     openGraph: {
       title: '위시리스트 | 빵그리의 오븐',
       description: '관심있는 상품이나 스토어를 위시리스트에 담아보세요.'
+    },
+    twitter: {
+      card: 'summary',
+      title: '위시리스트 | 빵그리의 오븐',
+      description: '관심있는 상품이나 스토어를 위시리스트에 담아보세요.'
     }
   },
   mypage: {
     title: '마이페이지',
     description: '빵그리의 오븐에 로그인 하고 다양한 서비스를 이용해 보세요.',
     openGraph: {
+      title: '마이페이지 | 빵그리의 오븐',
+      description: '빵그리의 오븐에 로그인 하고 다양한 서비스를 이용해 보세요.'
+    },
+    twitter: {
+      card: 'summary',
       title: '마이페이지 | 빵그리의 오븐',
       description: '빵그리의 오븐에 로그인 하고 다양한 서비스를 이용해 보세요.'
     }
@@ -76,7 +102,18 @@ const generateProductDetailMetadata = async ({
       images: [
         {
           url: product.profile,
-          alt: 'product image'
+          alt: `${product.title} 상품 이미지`
+        }
+      ]
+    },
+    twitter: {
+      card: 'summary',
+      title: '빵그리의 오븐',
+      description: `[${store.title}] ${product.title}`,
+      images: [
+        {
+          url: product.profile,
+          alt: `${product.title} 상품 이미지`
         }
       ]
     }
@@ -98,7 +135,18 @@ const generateStoreDetailMetadata = async ({
       images: [
         {
           url: storeInfo.profile,
-          alt: 'store image'
+          alt: `${storeInfo.storeName} 가게 로고`
+        }
+      ]
+    },
+    twitter: {
+      card: 'summary',
+      title: '빵그리의 오븐',
+      description: `[${storeInfo.storeName}] ${storeInfo.introduce}`,
+      images: [
+        {
+          url: storeInfo.profile,
+          alt: `${storeInfo.storeName} 가게 로고`
         }
       ]
     }

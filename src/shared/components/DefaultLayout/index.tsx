534981c4eccbe76f5fc3a3880e02aebc0218f425
jijiseong <jiseong5036@gmail.com>
2024-09-08 13:09:40 +0900

    feat: shadow 적용

--- src/shared/components/DefaultLayout/index.tsx
+++ src/shared/components/DefaultLayout/index.tsx
@@ -8,18 +8,13 @@ interface Props {
 }
 
 const DefaultLayout = ({ main, header, footer }: Props) => (
-  <div className="flex flex-col min-h-screen w-full">
-    {header && (
-      <header className="sticky top-0 max-w-[600px] w-full mx-auto z-header">{header}</header>
-    )}
-    <main id={ELEMENT_ID.main} className="max-w-[600px] w-full mx-auto relative flex-1">
+  <div className="flex flex-col min-h-screen w-full max-w-[600px] mx-auto shadow-lg">
+    {header && <header className="sticky top-0  w-full mx-auto z-header">{header}</header>}
+    <main id={ELEMENT_ID.main} className=" w-full mx-auto relative flex-1">
       {main}
     </main>
     {footer && (
-      <footer
-        id={ELEMENT_ID.footer}
-        className="max-w-[600px] w-full mx-auto shrink-0 sticky bottom-0 z-footer"
-      >
+      <footer id={ELEMENT_ID.footer} className=" w-full mx-auto shrink-0 sticky bottom-0 z-footer">
         {footer}
       </footer>
     )}

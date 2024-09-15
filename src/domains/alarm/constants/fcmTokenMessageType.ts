e62519fde76edf492aadca98bf170bdc694fec25
jijiseong <jiseong5036@gmail.com>
2024-09-15 16:27:26 +0900

    refactor: 필요없는 파일 삭제, message type 모아두기

--- src/domains/alarm/constants/fcmTokenMessageType.ts
+++
@@ -1,6 +0,0 @@
-export const FCM_TOKEN = {
-  default: 'fcmToken',
-  getFcmToken: 'fcmToken/getFcmToken',
-  getSucceed: 'fcmToken/getSucceed',
-  getFailed: 'fcmToken/getFailed'
-};
--- src/shared/constants/message.ts
+++ src/shared/constants/message.ts
@@ -1,1 +1,8 @@
 export const LOGIN_TYPE = 'login';
+
+export const FCM_TYPE = {
+  default: 'fcmToken',
+  getFcmToken: 'fcmToken/getFcmToken',
+  getSucceed: 'fcmToken/getSucceed',
+  getFailed: 'fcmToken/getFailed'
+};

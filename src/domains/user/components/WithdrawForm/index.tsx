703455ef48c9c9b71ad0ba439661fe72603d1352
hyejin <yhj970102@gmail.com>
2024-09-26 14:59:24 +0900

    🔨 Modify:  '중복' -> '다중' 워딩 변경

--- src/domains/user/components/WithdrawForm/index.tsx
+++ src/domains/user/components/WithdrawForm/index.tsx
@@ -26,7 +26,8 @@ const WithdrawForm = () => {
       <div>
         <p className="mb-[8px] typo-title-14-semibold">
           계정을 삭제하는 이유를 알려주세요😢
-          <span className="typo-body-12-semibold text-gray-400">(중복선택가능)</span>
+          <span className="typo-title-14-semibold text-primaryOrangeRed">*&nbsp;</span>
+          <span className="typo-body-12-semibold text-gray-400">(다중 선택 가능)</span>
         </p>
         <DeleteReasonList />
         <div className="flex justify-center mt-[20px] mb-[32px]">

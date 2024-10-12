92c2b9e092f0da5c5f987e4a126468e3039cedc7
Wlfjd <leejiwon813@naver.com>
2024-10-12 13:16:09 +0900

    🐛 Fix :  useGetReviewRatingQuery queryKey 변경

--- src/domains/review/queries/useGetReviewRatingQuery.ts
+++ src/domains/review/queries/useGetReviewRatingQuery.ts
@@ -3,7 +3,7 @@ import { reviewQueryKey } from '@/shared/queries/queryKey';
 import reviewService from './service';
 
 const useGetReviewRatingQuery = (boardId: number) => {
-  const queryKey = reviewQueryKey.detail(boardId);
+  const queryKey = reviewQueryKey.rating(boardId);
   const queryFn = () => reviewService.getReviewRating(boardId);
   return useQuery({
     queryKey,

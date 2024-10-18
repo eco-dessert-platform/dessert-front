102dfc2625f7e87aef99467e378b2ce6a729dbd0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-10-18 16:41:54 +0900

    🐛 Fix : 찜 > API 성공 멘트 & 삭제 후 리스트 리패칭 (#534)

--- src/domains/wish/components/common/FolderThumbnail.tsx
+++ src/domains/wish/components/common/FolderThumbnail.tsx
@@ -2,7 +2,6 @@ import ImageWithFallback from '@/shared/components/ImageWithFallback';
 import SadBbangleBox from '@/shared/components/SadBbangleBox';
 import { BbangleIcon } from '@/shared/components/icons';
 import { BLUR_DATA_URL } from '@/shared/constants/blurDataUrl';
-import Image from 'next/image';
 import { twMerge } from 'tailwind-merge';
 
 interface Props {
@@ -46,7 +45,19 @@ const FolderThumbnail = ({ thumbnailList, size = 'lg' }: Props) => {
       )}
       {hasThumbnail && thumbnailCount === 1 && (
         <div key={thumbnailList[0]} className="relative size-full">
-          <Image src={thumbnailList[0]} fill alt="thumbnail" />
+          <ImageWithFallback
+            src={thumbnailList[0]}
+            alt="상품사진"
+            placeholder="blur"
+            blurDataURL={BLUR_DATA_URL}
+            className="object-cover rounded-[6px]"
+            fill
+            fallback={
+              <SadBbangleBox className="border bg-gray-50 rounded-[6px] size-full typo-body-12-regular">
+                no image
+              </SadBbangleBox>
+            }
+          />
         </div>
       )}
       {!hasThumbnail && <BbangleIcon shape="smile" className="size-[80px]" />}

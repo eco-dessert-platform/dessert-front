7af14488b414f4cb3afa6265a2200091ea7b4342
Wlfjd <leejiwon813@naver.com>
2024-10-22 18:35:16 +0900

    🚑️ Hotfix:  구글로그인 시에도 외부로 리다이렉트

--- src/app/mypage/login/google-login/page.tsx
+++ src/app/mypage/login/google-login/page.tsx
@@ -3,9 +3,30 @@
 import Loading from '@/shared/components/Loading';
 import { LOGIN_TYPE } from '@/shared/constants/message';
 import { APP_URL } from '@/shared/constants/url';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const GoogleLoginLoadingPage = () => {
+  const [showRedirectMessage, setShowRedirectMessage] = useState(false);
+
+  useEffect(() => {
+    const isInKakaoInAppBrowser = () => {
+      const userAgent = navigator.userAgent || window.opera;
+      return /KAKAOTALK/i.test(userAgent);
+    };
+
+    if (isInKakaoInAppBrowser()) {
+      setShowRedirectMessage(true);
+    }
+  }, []);
+
+  const handleOpenInExternalBrowser = () => {
+    const currentUrl = window.location.href;
+    const url = new URL(currentUrl);
+    const cleanUrl = `${url.origin}/mypage/login`;
+    const encodedUrl = encodeURIComponent(cleanUrl);
+
+    window.location.href = `kakaotalk://web/openExternal?url=${encodedUrl}`;
+  };
   useEffect(() => {
     const { hash } = window.location;
     const params = new URLSearchParams(hash.substring(1));
@@ -26,7 +47,20 @@ const GoogleLoginLoadingPage = () => {
     window.close();
   });
 
-  return <Loading />;
+  return showRedirectMessage ? (
+    <div style={{ marginTop: '20px', textAlign: 'center' }}>
+      <p>이 페이지는 외부 브라우저에서 열어야 합니다.</p>
+      <button
+        type="button"
+        onClick={handleOpenInExternalBrowser}
+        style={{ padding: '10px 20px', background: 'gray', fontSize: '16px' }}
+      >
+        외부 브라우저로 열기
+      </button>
+    </div>
+  ) : (
+    <Loading />
+  );
 };
 
 export default GoogleLoginLoadingPage;

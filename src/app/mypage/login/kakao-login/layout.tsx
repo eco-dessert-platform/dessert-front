32d12ae7de7ed34e0d8193e6cb15d2e3a0b64ce6
Wlfjd <leejiwon813@naver.com>
2024-10-22 17:04:59 +0900

    🔖 Release :  리다이렉트 방식 변경

--- src/app/mypage/login/kakao-login/layout.tsx
+++ src/app/mypage/login/kakao-login/layout.tsx
@@ -1,6 +1,35 @@
 'use client';
 
-import { PropsWithChildren, Suspense } from 'react';
+import { PropsWithChildren, Suspense, useEffect } from 'react';
 
-const Layout = ({ children }: PropsWithChildren) => <Suspense>{children}</Suspense>;
+const Layout = ({ children }: PropsWithChildren) => {
+  useEffect(() => {
+    function isInKakaoInAppBrowser() {
+      const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      return /KAKAOTALK/i.test(userAgent);
+    }
+
+    function redirectToExternalBrowser() {
+      if (isInKakaoInAppBrowser()) {
+        const currentUrl = window.location.href;
+        const userAgent = navigator.userAgent || window.opera;
+        const isiOS = /iPhone|iPad|iPod/i.test(userAgent);
+
+        if (isiOS) {
+          // iOS 기기에서는 사파리로 열도록 처리
+          window.open(currentUrl, '_blank');
+        } else {
+          // Android 기기에서는 Chrome으로 열도록 처리
+          window.location.href =
+            `intent:${  currentUrl  }#Intent;scheme=https;package=com.android.chrome;end;`;
+        }
+      }
+    }
+
+    redirectToExternalBrowser();
+  }, []);
+
+  return <Suspense>{children}</Suspense>;
+};
+
 export default Layout;
--- src/app/mypage/login/kakao-login/page.tsx
+++ src/app/mypage/login/kakao-login/page.tsx
@@ -4,22 +4,11 @@ import Loading from '@/shared/components/Loading';
 import { LOGIN_TYPE } from '@/shared/constants/message';
 import { APP_URL } from '@/shared/constants/url';
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 const KakaoLoginLoadingPage = () => {
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
-
-  const [isKakaoInAppBrowser, setIsKakaoInAppBrowser] = useState(false);
-
-  useEffect(() => {
-    const useragt = navigator.userAgent.toLowerCase();
-    const isKakaoInApp = useragt.includes('kakaotalk');
-
-    if (isKakaoInApp) {
-      setIsKakaoInAppBrowser(true);
-    }
-  }, []);
 
   useEffect(() => {
     if (!code) return;
@@ -33,27 +22,6 @@ const KakaoLoginLoadingPage = () => {
     window.opener.postMessage(message, window.location.origin);
     window.close();
   }, [code]);
-
-  if (isKakaoInAppBrowser) {
-    return (
-      <div>
-        <p>현재 페이지는 카카오톡 인앱 브라우저에서 원활히 작동하지 않습니다.</p>
-        {/* 외부 브라우저로 강제 이동 (새 창 열기) */}
-        <a href={`${APP_URL}`} target="_blank" rel="noopener noreferrer">
-          외부 브라우저로 이동하기
-        </a>
-        <p>또는 아래 버튼을 클릭하세요:</p>
-        <button
-          type="button"
-          onClick={() => {
-            window.open(`${APP_URL}`, '_blank');
-          }}
-        >
-          외부 브라우저로 강제 이동
-        </button>
-      </div>
-    );
-  }
 
   return <Loading />;
 };

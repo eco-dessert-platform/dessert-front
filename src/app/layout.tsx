468e38d07e63792d69a09213fd7789f1804a1f0f
Wlfjd <leejiwon813@naver.com>
2024-10-22 14:39:35 +0900

    🚑️ Hotfix:  리다이렉션 코드  kakao-login쪽으로 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/global/global.css';
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode } from 'react';
 import localFont from 'next/font/local';
 import KaKaoChatScript from '@/global/KaKaoChatScript';
 import RootLayoutProvider from '@/global/RootLayoutProvider';
@@ -17,60 +17,22 @@ const pretendard = localFont({
 
 export const metadata = getStaticMetadata('root');
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
-  const [isKakaoInApp, setIsKakaoInApp] = useState(false);
-  useEffect(() => {
-    // 카카오톡 인앱 브라우저 감지
-    const useragt = navigator.userAgent.toLowerCase();
-    const isKakaoInAppBrowser = useragt.match(/kakaotalk/i);
-
-    if (isKakaoInAppBrowser) {
-      setIsKakaoInApp(true);
-      const currentUrl = window.location.href;
-
-      setTimeout(() => {
-        window.location.replace(
-          `kakaotalk://web/openExternal?url=${  encodeURIComponent(currentUrl)}`
-        );
-      }, 100);
-    }
-  }, []);
-
-  if (isKakaoInApp) {
-    return (
-      <div>
-        <p>현재 카카오톡 인앱 브라우저에서는 페이지가 올바르게 작동하지 않습니다.</p>
-        <p>
-          외부 브라우저에서 열어주세요.{' '}
-          <a href={window.location.href} target="_blank" rel="noreferrer">
-            여기
-          </a>{' '}
-          를 눌러서 크롬, 사파리 또는 삼성 인터넷에서 다시 열 수 있습니다.
-        </p>
-      </div>
-    );
-  }
-
-  return (
-    <html lang="ko">
-      <head>
-        <meta
-          name="google-site-verification"
-          content="PrpME1IVVESozFHxEKcgSVkL8preaJpDFYJ5Rtsjygk"
-        />
-      </head>
-      <body className={pretendard.className}>
-        <RootLayoutProvider>
-          <SilentLogin />
-          <ReceiveMessageFromApp />
-          {children}
-          <AlertContainer />
-        </RootLayoutProvider>
-        <KaKaoChatScript />
-        <GAScript />
-      </body>
-    </html>
-  );
-};
+const RootLayout = ({ children }: { children: ReactNode }) => (
+  <html lang="ko">
+    <head>
+      <meta name="google-site-verification" content="PrpME1IVVESozFHxEKcgSVkL8preaJpDFYJ5Rtsjygk" />
+    </head>
+    <body className={pretendard.className}>
+      <RootLayoutProvider>
+        <SilentLogin />
+        <ReceiveMessageFromApp />
+        {children}
+        <AlertContainer />
+      </RootLayoutProvider>
+      <KaKaoChatScript />
+      <GAScript />
+    </body>
+  </html>
+);
 
 export default RootLayout;
--- src/app/mypage/login/kakao-login/layout.tsx
+++ src/app/mypage/login/kakao-login/layout.tsx
@@ -1,6 +1,25 @@
 'use client';
 
-import { PropsWithChildren, Suspense } from 'react';
+import { PropsWithChildren, useEffect, Suspense } from 'react';
 
-const Layout = ({ children }: PropsWithChildren) => <Suspense>{children}</Suspense>;
+const Layout = ({ children }: PropsWithChildren) => {
+  useEffect(() => {
+    const useragt = navigator.userAgent.toLowerCase();
+    const isKakaoInAppBrowser = useragt.match(/kakaotalk/i);
+
+    if (isKakaoInAppBrowser) {
+      const currentUrl = window.location.href;
+
+      // 외부 브라우저로 리디렉션 시도
+      setTimeout(() => {
+        window.location.replace(
+          `kakaotalk://web/openExternal?url=${  encodeURIComponent(currentUrl)}`
+        );
+      }, 100); // 약간의 지연 후 리디렉션 시도
+    }
+  }, []);
+
+  return <Suspense>{children}</Suspense>;
+};
+
 export default Layout;

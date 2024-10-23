0b19f49517c05259a02d6ed1524fc61ce8bfa2a6
Wlfjd <leejiwon813@naver.com>
2024-10-23 14:05:44 +0900

    💄 Design:  storename , heart svg 디자인 수정

--- src/app/(with-navigation)/page.tsx
+++ src/app/(with-navigation)/page.tsx
@@ -1,5 +1,4 @@
 import HomeLogoSection from '@/blocks/home/HomeLogoSection';
-import Banner from '@/blocks/home/Banner';
 import Search from '@/blocks/home/SearchSection';
 import CategorySection from '@/blocks/home/CategorySection';
 import BestProductsSection from '@/blocks/home/BestProductsSection';
@@ -7,15 +6,15 @@ import RecommendationSection from '@/blocks/home/RecommendationSection';
 import TopButton from '@/shared/components/TopButton';
 
 const Home = () => (
-    <>
-      <HomeLogoSection />
-      <Search />
-      <Banner />
-      <CategorySection />
-      <RecommendationSection />
-      <BestProductsSection />
-      <TopButton />
-    </>
-  );
+  <>
+    <HomeLogoSection />
+    <Search />
+    {/* <Banner /> */}
+    <CategorySection />
+    <RecommendationSection />
+    <BestProductsSection />
+    <TopButton />
+  </>
+);
 
 export default Home;

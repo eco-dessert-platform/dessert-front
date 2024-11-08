aa35e32ce23d826c6bcf68e272964d621ec3686c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-11-08 11:18:15 +0900

    ✨ Feat :  Middleware.ts 추가

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const middleware = async (request: NextRequest) => {
+  const { pathname } = request.nextUrl;
+  const accessToken = cookies().get('accessToken')?.value || null;
+
+  if (pathname.startsWith('/review') && !accessToken) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  return NextResponse.next();
+};

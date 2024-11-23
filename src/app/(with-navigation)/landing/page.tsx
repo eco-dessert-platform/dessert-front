3d9cf969ebc74da52133503732ab5a522d870650
JIWON <leejiwon813@naver.com>
2024-11-23 09:22:36 +0900

    배포 (#566)
    Co-authored-by: HyeJin <yhj970102@gmail.com>
    Co-authored-by: JiSeong Park <jiseong5036@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>

--- src/app/(with-navigation)/landing/page.tsx
+++ src/app/(with-navigation)/landing/page.tsx
@@ -3,15 +3,18 @@ import Image from 'next/image';
 import { BLUR_DATA_URL } from '@/shared/constants/blurDataUrl';
 
 const LandingPage = () => (
-  <Image
-    src="/assets/images/banner-detail.png"
-    placeholder="blur"
-    blurDataURL={BLUR_DATA_URL}
-    priority
-    alt="banner"
-    width={600}
-    height={600}
-  />
+  <div className="relative">
+    <Image
+      src="/assets/images/banner-detail.png"
+      placeholder="blur"
+      blurDataURL={BLUR_DATA_URL}
+      unoptimized
+      priority
+      alt="banner"
+      width={600}
+      height={600}
+    />
+  </div>
 );
 
 export default LandingPage;

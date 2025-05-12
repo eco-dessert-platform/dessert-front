0ea972d5728fb76f9c4e767aee0b3eff75ccc74f
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-05-12 22:54:58 +0900

    🎨 Style :  카테고리 버튼 스타일 수정

--- src/blocks/home/CategorySection/CategoryButton.tsx
+++ src/blocks/home/CategorySection/CategoryButton.tsx
@@ -19,11 +19,11 @@ const CategoryButton = ({ name, icon }: CategoryButtonProps) => {
   return (
     <Link
       href={PATH.mainProductList}
-      className="flex flex-col justify-between items-center gap-1 py-[11px] hover:opacity-70 transition-opacity"
+      className="flex flex-col gap-1 items-center py-[11px] px-[22px] hover:opacity-70 transition-opacity"
       onClick={handleClickButton}
     >
       <div className="w-[46px] h-[46px] bg-gray-100 rounded-[16px] p-[11px]">{icon}</div>
-      <div className="text-gray-800 typo-body-12-regular">{name}</div>
+      <div className="text-gray-800 typo-body-12-regular whitespace-nowrap">{name}</div>
     </Link>
   );
 };

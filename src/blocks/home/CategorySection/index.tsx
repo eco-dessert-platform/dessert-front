a2e2e3f675cdd6c2c354d506501f50bc5afb3e8f
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-05-12 23:00:26 +0900

    📦️ Chore :  테스트 코드 제거

--- src/blocks/home/CategorySection/index.tsx
+++ src/blocks/home/CategorySection/index.tsx
@@ -6,15 +6,12 @@ import Slider from '@/blocks/home/CategorySection/Slider';
 import { CATEGORY } from '@/domains/home/constants/category';
 
 const CategorySection = () => (
-    <PaddingWrapper>
-      <Slider className="w-full h-full flex justify-center overflow-x-auto flex scroll-smooth clean-scrollbar">
-        {CATEGORY.map((category) => (
-          <CategoryButton key={category.id} name={category.name} icon={category.icon} />
-        ))}
-        {CATEGORY.map((category) => (
-          <CategoryButton key={`${category.id  }####`} name={category.name} icon={category.icon} />
-        ))}
-      </Slider>
-    </PaddingWrapper>
-  );
+  <PaddingWrapper>
+    <Slider className="w-full h-full flex justify-center overflow-x-auto flex scroll-smooth clean-scrollbar">
+      {CATEGORY.map((category) => (
+        <CategoryButton key={category.id} name={category.name} icon={category.icon} />
+      ))}
+    </Slider>
+  </PaddingWrapper>
+);
 export default CategorySection;

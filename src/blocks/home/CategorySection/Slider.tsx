809e517e32d6cf56ef491e371a23a91cdd6446bb
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-05-12 22:53:31 +0900

    ✨ Feat :  스크롤 가능하게 목록을 보여주는 Slider 제작

---
+++ src/blocks/home/CategorySection/Slider.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from 'react';
+
+interface SliderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Slider = ({ children, className }: SliderProps) => {
+  const slideRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const slide = slideRef.current;
+    if (!slide) return;
+
+    const handleWheel = (e: WheelEvent) => {
+      if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
+        e.preventDefault();
+        slide.scrollLeft += e.deltaY;
+      }
+    };
+
+    slide.addEventListener('wheel', handleWheel, { passive: false });
+
+    return () => {
+      slide.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
+
+  return (
+    <div ref={slideRef} className={className}>
+      {children}
+    </div>
+  );
+};
+
+export default Slider;
--- src/blocks/home/CategorySection/index.tsx
+++ src/blocks/home/CategorySection/index.tsx
@@ -2,13 +2,19 @@
 
 import PaddingWrapper from '@/shared/components/PaddingWrapper';
 import CategoryButton from '@/blocks/home/CategorySection/CategoryButton';
+import Slider from '@/blocks/home/CategorySection/Slider';
 import { CATEGORY } from '@/domains/home/constants/category';
 
 const CategorySection = () => (
-  <PaddingWrapper className="grid grid-cols-3">
-    {CATEGORY.map((category) => (
-      <CategoryButton key={category.id} name={category.name} icon={category.icon} />
-    ))}
-  </PaddingWrapper>
-);
+    <PaddingWrapper>
+      <Slider className="w-full h-full flex justify-center overflow-x-auto flex scroll-smooth clean-scrollbar">
+        {CATEGORY.map((category) => (
+          <CategoryButton key={category.id} name={category.name} icon={category.icon} />
+        ))}
+        {CATEGORY.map((category) => (
+          <CategoryButton key={`${category.id  }####`} name={category.name} icon={category.icon} />
+        ))}
+      </Slider>
+    </PaddingWrapper>
+  );
 export default CategorySection;

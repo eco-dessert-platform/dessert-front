2e9567c903d8cbc26cd2ced51a063ef5a3122bfe
joonho0410 <76806109+joonho0410@users.noreply.github.com>
2025-05-16 18:53:59 +0900

    ✨ Feat: 큐레이션 목록 변경 (#581)
    Co-authored-by: Junho Jeon <junhjeon@c1r1s5.42seoul.kr>

--- src/blocks/home/CategorySection/index.tsx
+++ src/blocks/home/CategorySection/index.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import PaddingWrapper from '@/shared/components/PaddingWrapper';
+import ScrollSlider from '@/shared/components/ScrollSlider';
 import CategoryButton from '@/blocks/home/CategorySection/CategoryButton';
 import { CATEGORY } from '@/domains/home/constants/category';
 
 const CategorySection = () => (
-  <PaddingWrapper className="grid grid-cols-3">
-    {CATEGORY.map((category) => (
-      <CategoryButton key={category.id} name={category.name} icon={category.icon} />
-    ))}
+  <PaddingWrapper>
+    <ScrollSlider className="w-full h-full flex justify-center overflow-x-auto scroll-smooth clean-scrollbar">
+      {CATEGORY.map((category) => (
+        <CategoryButton key={category.id} name={category.name} icon={category.icon} />
+      ))}
+    </ScrollSlider>
   </PaddingWrapper>
 );
 export default CategorySection;

49209d72d3ae338821cf792d91bd88098090d0e7
hyeone999 <caps026@naver.com>
2025-05-20 08:24:38 +0900

    ♻️ Refactor :  닉네임 중복확인 코드 개선 및 로직 구분

--- src/domains/user/components/common/NickNameInput.tsx
+++ src/domains/user/components/common/NickNameInput.tsx
@@ -6,41 +6,46 @@ import ButtonNewver from '@/shared/components/ButtonNewver';
 import Input from '@/shared/components/Input';
 import useNicknameDoubleCheckMutation from '../../queries/useNicknameDoubleCheckMutation';
 
+const MAX_LENGTH = 20;
+
 const NicknameInput = () => {
   const inputId = useId();
-  const { mutate, data, reset } = useNicknameDoubleCheckMutation();
+  const { mutate, data } = useNicknameDoubleCheckMutation();
   const { watch, setValue } = useFormContext();
+
   const [isNickNameTouched, setIsNickNameTouched] = useState(false);
+  const [isNickNameMessageVisible, setIsNickNameMessageVisible] = useState(false);
+  const [lastCheckedNickname, setLastCheckedNickname] = useState('');
 
   const nickname = watch('nickname');
   const nicknameLength = nickname.length;
   const nicknameClass = nicknameLength === 0 ? 'text-gray-400' : 'text-gray-600';
-  const MAX_LENGTH = 20;
+
+  const checkResult = (isValid: boolean) => {
+    setValue('isNickDoubleChecked', isValid);
+    setLastCheckedNickname(nickname);
+    setIsNickNameMessageVisible(true);
+  };
 
   const nickDoubleCheck = () => {
-    if (!isNickNameTouched) return;
+    if (!isNickNameTouched || nickname === lastCheckedNickname) return;
 
-    mutate(nickname || '', {
+    mutate(nickname, {
       onSuccess: (res) => {
-        if (res.isValid) {
-          setValue('nickname', nickname, { shouldDirty: true });
-          setValue('isNickDoubleChecked', true);
-        } else {
-          setValue('isNickDoubleChecked', false);
-        }
+        checkResult(res.isValid);
       },
       onError: (error) => {
         console.error('닉네임 중복 체크 실패:', error);
-        setValue('isNickDoubleChecked', false);
+        checkResult(false);
       }
     });
   };
 
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsNickNameTouched(true);
+    setIsNickNameMessageVisible(false);
     setValue('nickname', e.target.value);
     setValue('isNickDoubleChecked', false);
-    reset();
   };
 
   return (
@@ -67,7 +72,7 @@ const NicknameInput = () => {
           </ButtonNewver>
         }
       />
-      {data?.message && (
+      {data?.message && isNickNameMessageVisible && (
         <div className="mt-[4px] float-start text-gray-600 typo-body-12-medium">{data.message}</div>
       )}
       <p className="mt-[4px] float-end text-gray-600 typo-body-12-medium">

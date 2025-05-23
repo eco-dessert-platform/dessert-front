c4b4feaed791eaf35a2b3e387f1ed6389313fdb4
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-05-23 18:11:16 +0900

    ✨ Feat :  새로운 API 요청함수, 반환 타입 생성

---
+++ src/domains/product/types/productInfoType.ts
@@ -0,0 +1,64 @@
+type Store = {
+  storeId: number;
+  storeTitle: string;
+  storeProfile: string;
+  isStoreWished: boolean;
+};
+
+type Board = {
+  boardId: number;
+  boardProfile: string;
+  boardTitle: string;
+  boardPrice: number;
+  purchaseUrl: string;
+  isSoldout: boolean;
+  deliveryFee: number;
+  freeShippingConditions: number;
+  discountRate: number;
+  isBoardWished: boolean;
+  boardImages: string[];
+  boardDetail: string;
+};
+
+type Nutrient = {
+  sugars: number;
+  protein: number;
+  carbohydrates: number;
+  fat: number;
+  weight: number | null;
+  calories: number | null;
+};
+
+type OrderType = {
+  orderType: 'WEEK';
+  monday: boolean;
+  tuesday: boolean;
+  wednesday: boolean;
+  thursday: boolean;
+  friday: boolean;
+  saturday: boolean;
+  sunday: boolean;
+  orderStartDate: string | null;
+  orderEndDate: string | null;
+};
+
+export type ProductType = {
+  id: number;
+  title: string;
+  glutenFreeTag: boolean;
+  highProteinTag: boolean;
+  sugarFreeTag: boolean;
+  veganTag: boolean;
+  ketogenicTag: boolean;
+  price: number;
+  nutrient: Nutrient;
+  isSoldout: boolean;
+  orderType: OrderType;
+  isBbangketting: boolean;
+};
+
+export interface IProductInfoType {
+  store: Store;
+  board: Board;
+  products: ProductType[];
+}

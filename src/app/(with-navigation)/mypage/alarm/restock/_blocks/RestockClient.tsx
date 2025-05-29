9fcecde97d363d15e66723881ef15e6665a537d9
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-05-29 11:24:46 +0900

    🐛 fix: 오류로 인해 추가했던 client 제거

--- src/app/(with-navigation)/mypage/alarm/restock/_blocks/RestockClient.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-
-import RestockProductList from '@/app/(with-navigation)/mypage/alarm/restock/_blocks/RestockProductList';
-
-const RestockClient = () => <RestockProductList />;
-
-export default RestockClient;
--- src/app/(with-navigation)/mypage/alarm/restock/page.tsx
+++ src/app/(with-navigation)/mypage/alarm/restock/page.tsx
@@ -1,18 +1,18 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { alarmQueryKey } from '@/domains/alarm/queries/queryKey';
 import alarmService from '@/domains/alarm/queries/service';
-import RestockClient from '@/app/(with-navigation)/mypage/alarm/restock/_blocks/RestockClient';
+import RestockProductList from '@/app/(with-navigation)/mypage/alarm/restock/_blocks/RestockProductList';
 
 const RestockPage = async () => {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: alarmQueryKey.list('restock'),
-    queryFn: () => alarmService.getAlarm({ pushCategory: 'restock' }),
+    queryFn: () => alarmService.getAlarm({ pushCategory: 'restock' })
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <RestockClient />
+      <RestockProductList />
     </HydrationBoundary>
   );
 };

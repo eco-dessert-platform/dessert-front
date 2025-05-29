9172920fca80a0e6dc896abc828e47bfcfbf57c8
hyeone999 <caps026@naver.com>
2025-05-29 16:02:54 +0900

    ✨ Feat :  카테고리 count API 수정

--- src/domains/product/queries/service.ts
+++ src/domains/product/queries/service.ts
@@ -37,11 +37,12 @@ class ProductService extends Service {
   async getBoardsCount({ filterValue }: { filterValue: Omit<IFilterType, 'sort'> }) {
     const filterValueQueryString = transformFilterValueToQueryString(filterValue);
 
-    const res = await this.fetchExtend.get(`/boards/count?${filterValueQueryString}`);
-    const { success, result, code, message }: ResultResponse<number> = await res.json();
+    const res = await this.fetchExtend.get(`/boards?${filterValueQueryString}`);
+    const { success, result, code, message }: ResultResponse<Cursor<Array<IProductType>>> =
+      await res.json();
 
     if (!res.ok || !success) throw new Error(ERROR_MESSAGE.api({ code, message }));
-    return result;
+    return result.totalCount;
   }
 
   async getStoreInfo(productId: number) {

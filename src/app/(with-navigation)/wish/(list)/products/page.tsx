26d5654a3475c4e9a0649618b1b27e9fece43e18
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-05-29 16:20:07 +0900

    fix: 오류로 인해 추가했던 client 제거

--- src/app/(with-navigation)/wish/(list)/products/page.tsx
+++ src/app/(with-navigation)/wish/(list)/products/page.tsx
@@ -1,19 +1,28 @@
-import { QueryClient, dehydrate } from '@tanstack/react-query';
+import { QueryClient, dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { wishQueryKey } from '@/domains/wish/queries/queryKey';
 import wishService from '@/domains/wish/queries/service';
-import WishProductsClient from '@/blocks/wish/(list)/products/WishProductsClient';
+import PaddingWrapper from '@/shared/components/PaddingWrapper';
+import WishFolderEditButtonSection from '@/blocks/wish/(list)/products/WishFolderEditButtonSection';
+import WishFolderGrid from '@/blocks/wish/(list)/products/WishFolderGrid';
 
 const WishProductsPage = async () => {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: wishQueryKey.folders(),
-    queryFn: () => wishService.getWishFolderList(),
+    queryFn: () => wishService.getWishFolderList()
   });
 
   const dehydratedState = dehydrate(queryClient);
 
-  return <WishProductsClient dehydratedState={dehydratedState} />;
+  return (
+    <PaddingWrapper>
+      <HydrationBoundary state={dehydratedState}>
+        <WishFolderEditButtonSection />
+        <WishFolderGrid />
+      </HydrationBoundary>
+    </PaddingWrapper>
+  );
 };
 
 export default WishProductsPage;

3a88ded2fd63f1ea603ab70b897f621ef3430727
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-05-30 10:42:33 +0900

    🐛 fix: 잘못 수정된 페이지 수정

--- src/app/mypage/registration/page.tsx
+++ src/app/mypage/registration/page.tsx
@@ -1,26 +1,5 @@
-'use client';
+import RegistrationForm from '@/domains/user/components/RegistrationForm';
 
-import { ReactNode } from 'react';
+const Registration = () => <RegistrationForm />;
 
-import DefaultLayout from '@/shared/components/DefaultLayout';
-import Header from '@/shared/components/Header';
-import { REGISTRATION_DEFAULT_VALUE } from '@/domains/user/constants/profile';
-
-import RegistrationFormProvider from './_blocks/RegistrationFormProvider';
-import ButtonSection from './_blocks/ButtonSection';
-
-interface Props {
-  children: ReactNode;
-}
-
-const Layout = ({ children }: Props) => (
-  <RegistrationFormProvider defaultValues={REGISTRATION_DEFAULT_VALUE}>
-    <DefaultLayout
-      header={<Header title="프로필 등록" />}
-      main={children}
-      footer={<ButtonSection />}
-    />
-  </RegistrationFormProvider>
-);
-
-export default Layout;
+export default Registration;

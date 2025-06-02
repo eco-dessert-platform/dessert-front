b7228bda6ad00709acef2e28a264b12bfa6635dc
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-06-02 17:04:36 +0900

    🐛 fix: meta-data 보여주는 방식 수정

--- src/shared/utils/metadata.ts
+++ src/shared/utils/metadata.ts
@@ -32,6 +32,9 @@ const staticMetadata: StaticMetadataType = {
     },
     description:
       '빵그리의 오븐은 건강을 소중히 여기는 이들에게 새로운 디저트 경험을 선사하고, 건강과 맛을 모두 충족시킬 수 있는 특별한 공간을 제공합니다.',
+    verification: {
+      google: 'PrpME1IVVESozFHxEKcgSVkL8preaJpDFYJ5Rtsjygk'
+    },
     openGraph: {
       title: '빵그리의 오븐',
       description:

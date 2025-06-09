8cf517fba6169ba89ae0a8e3bff703e12e71160c
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-06-09 13:27:25 +0900

    🐛 fix: tailwind base 병합

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   // tailwind CSS 를 적용할 경로를 app폴더내 모든 디렉토리내 js,ts,jsx,tsx파일에 적용
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-
   future: {
     hoverOnlyWhenSupported: true
   },

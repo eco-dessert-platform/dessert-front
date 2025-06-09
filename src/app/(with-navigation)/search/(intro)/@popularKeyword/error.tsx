46b8a2a07d76447bbde21357544b452183eab3ea
ravenkim <ravenkim97@naver.com>
2025-06-09 17:39:49 +0900

    🐛 fix:  router.refresh(); 통해 캐쉬 초기화

--- src/app/(with-navigation)/search/(intro)/@popularKeyword/error.tsx
+++ src/app/(with-navigation)/search/(intro)/@popularKeyword/error.tsx
@@ -1,26 +1,34 @@
 'use client';
 
 import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Button from '@/shared/components/Button';
 import { RefreshIcon } from '@/shared/components/icons';
 
 const PopularKeywordError = ({
-  error,
-  reset
-}: {
+                               error,
+                               reset
+                             }: {
   error: Error & { digest?: string };
   reset: () => void;
 }) => {
+  const router = useRouter();
+
   useEffect(() => {
     console.error(error);
   }, [error]);
+
+  const handleReset = () => {
+    reset();
+    router.refresh();
+  };
 
   return (
     <div className="flex flex-col items-center typo-title-14-regular text-gray-500">
       <p>네트워크 문제로 일시적인 오류가 발생했어요.</p>
       <p>다시 시도해주세요.</p>
       <Button
-        onClick={reset}
+        onClick={handleReset}
         variants="primary-white"
         className="flex gap-x-[2px] justify-center items-center mt-[16px] p-[16px] w-[149px] typo-title-16-medium"
       >

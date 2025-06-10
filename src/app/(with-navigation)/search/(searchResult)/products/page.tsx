89a6f505613c8111b8564c5e51bfca1652f6e6e1
hyeone999 <caps026@naver.com>
2025-06-10 18:11:56 +0900

    🔨 Modify:  Next 15 searchParams 문서 참고하여 코드 수정

--- src/app/(with-navigation)/search/(searchResult)/products/page.tsx
+++ src/app/(with-navigation)/search/(searchResult)/products/page.tsx
@@ -1,14 +1,15 @@
-'use client';
-
 import SortingFilterSection from '@/blocks/main/(list)/SortingFilterSection';
 import SearchProductList from '@/blocks/search/products/SearchProductList';
 import { FILTER_FAMILY_ID } from '@/domains/product/constants/filterFamilyID';
 import TopButton from '@/shared/components/TopButton';
-import { useSearchParams } from 'next/navigation';
 
-const SearchProducts = () => {
-  const params = useSearchParams();
-  const keyword = params.get('query') ?? '';
+interface SearchProductsProps {
+  searchParams: { query?: string };
+}
+
+const SearchProducts = async ({ searchParams }: SearchProductsProps) => {
+  const params = await searchParams;
+  const keyword = params.query ?? '';
 
   return (
     <>

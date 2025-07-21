f25603106e5245a40a6449971fb1aa77aedc69cd
Junho jeon <junhjeon@Junhoui-MacBookAir.local>
2025-07-21 17:24:25 +0900

    ✨ Feat :  useSearchParams를 위한 Suspense추가

--- src/domains/user/queries/useLoginMutation.ts
+++ src/domains/user/queries/useLoginMutation.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { useMutation } from '@tanstack/react-query';
 import { useRouter, useSearchParams } from 'next/navigation';
 import useToastNewVer from '@/shared/hooks/useToastNewVer';

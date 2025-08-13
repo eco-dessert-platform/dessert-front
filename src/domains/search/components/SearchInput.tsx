1399b2de441d503caad4ea0a1ddfcdfa01cb2844
ravenkim <ravenkim97@naver.com>
2025-08-13 13:44:15 +0900

    🐛 fix: input 배경 색 수정

--- src/domains/search/components/SearchInput.tsx
+++ src/domains/search/components/SearchInput.tsx
@@ -19,16 +19,16 @@ const SearchInput = ({
 }: SearchInputProps) => (
   <div
     className={cn(
-      'flex w-full items-center gap-[6px] rounded-[50px] px-[16px] py-[10px]',
-      warning ? 'border-secondary-orange-red bg-secondary-pink border' : 'bg-red-gray-30'
+      ' flex w-full items-center gap-[6px] rounded-[50px] px-[16px] py-[10px]',
+      warning ? 'border-[1px] border-secondaryOrangeRed bg-secondaryPink' : 'bg-redGray-30'
     )}
   >
     {warning ? <SearchIcon shape="red-16" /> : <SearchIcon shape="gray-16" />}
     <input
       type="text"
       className={cn(
-        'typo-title-14-medium flex-1 rounded-none border-none p-0 text-gray-900 outline-hidden',
-        warning ? 'bg-secondary-pink' : 'bg-red-gray-30'
+        ' typo-title-14-medium flex-1 rounded-none border-none p-0  bg-pink-500 text-gray-900 outline-hidden',
+        warning ? 'bg-secondaryPink' : 'bg-redGray-30'
       )}
       value={value}
       onChange={onChange}

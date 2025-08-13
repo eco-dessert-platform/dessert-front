f073ab5972d1f9d17dc8c2175f1942961c491cfa
ravenkim <ravenkim97@naver.com>
2025-08-13 13:56:39 +0900

    🐛 fix: 디자인 변경 요구사항 반영

--- src/app/main/products/[productId]/info/_blocks/ProductOptionsSection/CategoryOption.tsx
+++ src/app/main/products/[productId]/info/_blocks/ProductOptionsSection/CategoryOption.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ArrowIcons from '@/shared/components/icons/ArrowIcons';
 import PaddingWrapper from '@/shared/components/PaddingWrapper';
@@ -12,25 +12,50 @@ interface Props {
   onClick: () => void;
 }
 
-const CategoryOption = ({ product, isExpended, onClick }: Props) => (
-  <PaddingWrapper
-    className={cn(
-      'typo-title-14-regular border-b border-gray-100 text-gray-800',
-      product.isSoldout && 'bg-gray-100'
-    )}
-  >
-    <button type="button" onClick={onClick} className="flex w-full items-center justify-between">
-      <div className="flex gap-x-[5px]">
-        <span>{product.isSoldout && '(품절)'}</span>
-        <span>{product.title}</span>
-          <IngredientInfo product={product} />
-      </div>
+const CategoryOption = ({ product, isExpended, onClick }: Props) => {
 
-      <span className={cn(isExpended && 'rotate-180 transition-all')}>
-        <ArrowIcons shape="large-down" />
-      </span>
-    </button>
-  </PaddingWrapper>
-);
+  const visible = !(
+    product.orderType.orderType === "WEEK" &&
+    product.orderType.monday &&
+    product.orderType.tuesday &&
+    product.orderType.wednesday &&
+    product.orderType.thursday &&
+    product.orderType.friday &&
+    product.orderType.saturday &&
+    product.orderType.sunday &&
+    product.nutrient.sugars === -1 &&
+    product.nutrient.protein === -1 &&
+    product.nutrient.carbohydrates === -1 &&
+    product.nutrient.fat === -1 &&
+    product.nutrient.weight === null &&
+    product.nutrient.calories === null
+  );
+
+
+  return (
+    <PaddingWrapper
+      className={cn(
+        'typo-title-14-regular border-b border-gray-100 text-gray-800',
+        product.isSoldout && 'bg-gray-100'
+      )}
+    >
+      <button type="button"  onClick={visible ? onClick : undefined}
+
+              className="flex w-full items-center justify-between">
+        <div className="flex gap-x-[5px]">
+          <span>{product.isSoldout && '(품절)'}</span>
+          <span>{product.title}</span>
+          <IngredientInfo product={product} />
+        </div>
+
+        {visible && (
+          <span className={cn(isExpended && 'rotate-180 transition-all')}>
+            <ArrowIcons shape="large-down" />
+          </span>
+        )}
+      </button>
+    </PaddingWrapper>
+  );
+};
 
 export default CategoryOption;

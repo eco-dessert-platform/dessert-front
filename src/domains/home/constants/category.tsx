b8a13dde3cd9204038d2ecfee97472a23d60a079
Wlfjd <leejiwon813@naver.com>
2026-03-18 19:26:57 +0900

    fix: category

--- src/domains/home/constants/category.tsx
+++ src/domains/home/constants/category.tsx
@@ -1,35 +1,14 @@
 import {
   AllIcon,
-  GlutenFreeIcon,
   LowFatIcon,
   ProteinIcon,
   VeganIcon
 } from '@/domains/product/assets/category/icons';
 
 export const CATEGORY = [
-  {
-    id: 1,
-    name: '새로 나왔어요',
-    icon: <AllIcon />
-  },
-  {
-    id: 2,
-    name: '전체',
-    icon: <ProteinIcon />
-  },
-  {
-    id: 3,
-    name: '단백질 듬뿍',
-    icon: <GlutenFreeIcon />
-  },
-  {
-    id: 4,
-    name: '칼로리 다운',
-    icon: <LowFatIcon />
-  },
-  {
-    id: 5,
-    name: '속 편한 즐거움',
-    icon: <VeganIcon />
-  }
+  { id: 1, name: '전체', icon: <AllIcon /> },
+  { id: 2, name: '단백질 듬뿍', icon: <ProteinIcon /> },
+
+  { id: 3, name: '칼로리 다운', icon: <LowFatIcon /> },
+  { id: 4, name: '속 편한 즐거움', icon: <VeganIcon /> }
 ];

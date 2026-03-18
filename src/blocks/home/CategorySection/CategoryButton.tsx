c43c3e3da691714c64a75fa883dace6d97f19fef
JIWON <leejiwon813@naver.com>
2026-03-18 19:48:45 +0900

    release (#649)

--- src/blocks/home/CategorySection/CategoryButton.tsx
+++ src/blocks/home/CategorySection/CategoryButton.tsx
@@ -1,37 +1,21 @@
 import Link from 'next/link';
 
-import PATH from '@/shared/constants/path';
-import { useFilter } from '@/shared/hooks/useFilter';
 import { FILTER_FAMILY_ID } from '@/domains/product/constants/filterFamilyID';
 import { INIT_FILTER_VALUE } from '@/domains/product/constants/filterValues';
+import PATH from '@/shared/constants/path';
+import { useFilter } from '@/shared/hooks/useFilter';
 
 function getCategoryFilterOption(name: string) {
   switch (name) {
-    case '새로 나왔어요':
-      return {
-        tags: ['전체'],
-        sort: '최신순'
-      };
     case '전체':
-      return {
-        tags: ['전체'],
-        sort: '추천순'
-      };
+      return { tags: ['전체'], sort: '최신순' };
+
     case '단백질 듬뿍':
-      return {
-        tags: ['고단백'],
-        sort: '추천순'
-      };
+      return { tags: ['고단백'], sort: '추천순' };
     case '칼로리 다운':
-      return {
-        tags: ['저당', '저지방'],
-        sort: '추천순'
-      };
+      return { tags: ['저당', '저지방'], sort: '추천순' };
     case '속 편한 즐거움':
-      return {
-        tags: ['비건', '글루텐프리'],
-        sort: '추천순'
-      };
+      return { tags: ['비건', '글루텐프리'], sort: '추천순' };
     default:
       return {};
   }
